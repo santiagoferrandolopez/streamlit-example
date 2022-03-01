a4cd3bb4da0e32c7c97d4055370d5f4bd7a6e383
Santiago Ferrando <50645104+santiagoferrandolopez@users.noreply.github.com>
2022-03-01 09:26:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 import numpy as np
-import warnings
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
-"""
+
 st.title("Ponsse Data Analysis")
