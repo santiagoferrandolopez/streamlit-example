231a19cc36f21ecec2050718f7ea494ce59a8fa8
Santiago Ferrando <50645104+santiagoferrandolopez@users.noreply.github.com>
2022-03-01 10:34:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 import numpy as np
 
 st.markdown("Ponsse Data Analysis")
+
+# Importing the dataset
+Cobra = "C:\\Users\\administra\\Ponsse\\cobra_hv.xlsx"
+Ergo = "C:\\Users\\administra\\Ponsse\\ergo_hv.xlsx"
+Ambas= "C:\\Users\\administra\\Ponsse\\cobra_ergo_hv.xlsx"
