ee62807d884b1cc0646ac955b9bd1851789c2866
Santiago Ferrando <50645104+santiagoferrandolopez@users.noreply.github.com>
2022-03-01 10:35:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,24 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-
-st.markdown("Ponsse Data Analysis")
+# Importing the libraries
+import warnings
+warnings.filterwarnings('ignore')
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+st.title("Ponsse Data Analysis")
 
 # Importing the dataset
 Cobra = "C:\\Users\\administra\\Ponsse\\cobra_hv.xlsx"
 Ergo = "C:\\Users\\administra\\Ponsse\\ergo_hv.xlsx"
 Ambas= "C:\\Users\\administra\\Ponsse\\cobra_ergo_hv.xlsx"
+
+df = pd.read_excel(Cobra)
+dff = pd.read_excel(Ergo)
+both=pd.read_excel(Ambas)
+
+sns.boxplot(df['lreal/hrmot'])
