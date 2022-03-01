30712031bcff620fb0815e555d883c6ee44ff862
Santiago Ferrando <50645104+santiagoferrandolopez@users.noreply.github.com>
2022-03-01 10:36:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 # Importing the libraries
-import warnings
-warnings.filterwarnings('ignore')
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
