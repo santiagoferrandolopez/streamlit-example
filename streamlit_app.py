b23d5a4ad3ad0785c577ace059373cbb0a42cb34
Santiago Ferrando <50645104+santiagoferrandolopez@users.noreply.github.com>
2022-03-07 06:30:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,7 @@ st.subheader("MOM analytics")
 st.text("What´s the hell is going on")
 #Displaying Colored Text/Boostraps Alert
 st.success("What")
+st.warning("Cuidado")
+st.info("Información")
+st.error("Error")
+st.exception("Excepción")
